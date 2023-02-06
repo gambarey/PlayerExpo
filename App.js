143ba0baa274123edca245645de9bf9a6d470696
Carlos Rodriguez <crribera@gmail.com>
2023-02-06 10:23:00 +0100

    audio file styling

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './assets/app/navigation/AppNavigator';
 import AudioProvider from './assets/app/context/AudioProvider';
+import { View } from 'react-native';
+import AudioListItem from './assets/app/components/AudioListItem';
 
 export default function App() {
   return (
-    <AudioProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AudioProvider>
+    // <AudioProvider>
+    //   <NavigationContainer>
+    //     <AppNavigator />
+    //   </NavigationContainer>
+    // </AudioProvider>
+    <View style={{marginTop: 50}}>
+      <AudioListItem />
+    </View>
   );
 }
 
