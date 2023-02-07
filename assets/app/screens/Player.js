11d8bce8757e5a093ce965bec9d87cb254b6f132
Carlos Rodriguez <crribera@gmail.com>
2023-02-07 17:30:52 +0100

    audio player UI buttons

--- assets/app/screens/Player.js
+++ assets/app/screens/Player.js
@@ -1,9 +1,9 @@
-//import liraries
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import color from '../misc/color';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Slider from '@react-native-community/slider';
+import PlayerButton from '../components/PlayerButton';
 
 const { width } = Dimensions.get('window');
 
@@ -26,6 +26,11 @@ const Player = () => {
                     minimumTrackTintColor={color.FONT_MEDIUM}
                     maximumTrackTintColor={color.ACTIVE_BG}
                 />
+                <View style={styles.audioControllers}>
+                    <PlayerButton iconType="backward" />
+                    <PlayerButton onPress={() => console.log("play pressed")} iconType="play" />
+                    <PlayerButton iconType="forward" />
+                </View>
             </View>
         </View>
     );
@@ -35,6 +40,13 @@ const Player = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    audioControllers: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        paddingBottom: 20,
+        marginHorizontal: 60
     },
     audioCount: {
         color: color.FONT_LIGHT,
