e0a189c80e89d36a181c8369ac2ea4d1c476e662
Carlos Rodriguez <crribera@gmail.com>
2023-02-07 11:21:55 +0100

    audio player UI icon central + file name + slider

--- assets/app/screens/Player.js
+++ assets/app/screens/Player.js
@@ -1,12 +1,32 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import color from '../misc/color';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Slider from '@react-native-community/slider';
 
-// create a component
+const { width } = Dimensions.get('window');
+
 const Player = () => {
     return (
         <View style={styles.container}>
-            <Text>Player</Text>
+            <Text style={styles.audioCount}>1 / 99</Text>
+            <View style={styles.midBannerContainer}>
+                <MaterialCommunityIcons
+                    name="music-circle"
+                    size={300}
+                    color={color.ACTIVE_BG} />
+            </View>
+            <View style={styles.audioPlayerContainer}>
+                <Text numberOfLines={1} style={styles.audioTitle} >Audio File Name</Text>
+                <Slider
+                    style={{ width: width, height: 40 }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    minimumTrackTintColor={color.FONT_MEDIUM}
+                    maximumTrackTintColor={color.ACTIVE_BG}
+                />
+            </View>
         </View>
     );
 };
@@ -14,10 +34,23 @@ const Player = () => {
 // define your styles
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    audioCount: {
+        color: color.FONT_LIGHT,
+        padding: 15,
+        textAlign: 'right',
+        fontSize: 14
+    },
+    midBannerContainer: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        alignItems: 'center'
+    },
+    audioTitle: {
+        color: color.FONT,
+        fontSize: 16,
+        padding: 15
     },
 });
 
