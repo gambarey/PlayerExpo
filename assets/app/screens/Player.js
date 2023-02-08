35f0ccb76544e19f8cbc984fe2d2ad74a173e90c
Carlos Rodriguez <crribera@gmail.com>
2023-02-08 20:35:05 +0100

    added prev/next functionality on player page

--- assets/app/screens/Player.js
+++ assets/app/screens/Player.js
@@ -5,7 +5,8 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import Slider from '@react-native-community/slider';
 import PlayerButton from '../components/PlayerButton';
 import { AudioContext } from '../context/AudioProvider';
-import { play, pause, resume } from '../misc/AudioController';
+import { play, pause, resume, playNext } from '../misc/AudioController';
+import { storeAudioForNextOpening } from '../misc/helper';
 
 const { width } = Dimensions.get('window');
 
@@ -54,70 +55,149 @@ const Player = () => {
         }
     }
 
+    const handleNext = async () => {
+        const { isLoaded } = await context.playbackObj.getStatusAsync();
+        const isLastAudio = context.currentAudioIndex + 1 === context.totalAudioCount;
+        let audio = context.audioFiles[context.currentAudioIndex + 1];
+        let index;
+        let status;
 
-            if (!context.currentAudio) {
-                return null;
+        if (!isLoaded && !isLastAudio) {
+            status = await play(context.playbackObj, audio.uri);
+            index = context.currentAudioIndex + 1;
+        }
+
+        if (isLoaded && !isLastAudio) {
+            status = await playNext(context.playbackObj, audio.uri);
+            index = context.currentAudioIndex + 1;
+        }
+
+        if (isLastAudio) {
+            index = 0;
+            audio = context.audioFiles[index];
+            if (isLoaded) {
+                status = await playNext(context.playbackObj, audio.uri);
+            } else {
+                status = await play(context.playbackObj, audio.uri);
             }
+        }
 
-            return (
-                <View style={styles.container}>
-                    <Text style={styles.audioIndex}>{`${context.currentAudioIndex + 1} / ${context.totalAudioCount}`}</Text>
-                    <View style={styles.midBannerContainer}>
-                        <MaterialCommunityIcons
-                            name="music-circle"
-                            size={300}
-                            color={context.isPlaying ? color.ACTIVE_BG : color.FONT_LIGHT} />
-                    </View>
-                    <View style={styles.audioPlayerContainer}>
-                        <Text numberOfLines={1} style={styles.audioTitle} >{context.currentAudio.filename}</Text>
-                        <Slider
-                            style={{ width: width, height: 40 }}
-                            minimumValue={0}
-                            maximumValue={1}
-                            value={calculateSeekBar()}
-                            minimumTrackTintColor={color.FONT_MEDIUM}
-                            maximumTrackTintColor={color.ACTIVE_BG}
-                        />
-                        <View style={styles.audioControllers}>
-                            <PlayerButton iconType="backward" />
-                            <PlayerButton onPress={handlePlayPause}
-                                iconType={context.isPlaying ? "pause" : "play"} />
-                            <PlayerButton iconType="forward" />
-                        </View>
-                    </View>
-                </View>
-            );
-        };
 
-        // define your styles
-        const styles = StyleSheet.create({
-            container: {
-                flex: 1
-            },
-            audioControllers: {
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-                alignItems: 'center',
-                paddingBottom: 20,
-                marginHorizontal: 60
-            },
-            audioCount: {
-                color: color.FONT_LIGHT,
-                padding: 15,
-                textAlign: 'right',
-                fontSize: 14
-            },
-            midBannerContainer: {
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-            },
-            audioTitle: {
-                color: color.FONT,
-                fontSize: 16,
-                padding: 15
-            },
+        context.updateState(context, {
+            soundObj: status,
+            currentAudio: audio,
+            isPlaying: true,
+            currentAudioIndex: index,
+            playbackObj: context.playbackObj,
         });
+        storeAudioForNextOpening(audio, index);
+    }
 
-        //make this component available to the app
-        export default Player;
+    const handlePrevious = async () => {
+        const { isLoaded } = await context.playbackObj.getStatusAsync();
+        const isFirstAudio = context.currentAudioIndex <= 0;
+        let audio = context.audioFiles[context.currentAudioIndex - 1];
+        let index;
+        let status;
+
+        if (!isLoaded && !isFirstAudio) {
+            status = await play(context.playbackObj, audio.uri);
+            index = context.currentAudioIndex - 1;
+        }
+
+        if (isLoaded && !isFirstAudio) {
+            status = await playNext(context.playbackObj, audio.uri);
+            index = context.currentAudioIndex - 1;
+        }
+
+        if (isFirstAudio) {
+            index = context.totalAudioCount - 1;
+            audio = context.audioFiles[index];
+            if (isLoaded) {
+                status = await playNext(context.playbackObj, audio.uri);
+            } else {
+                status = await play(context.playbackObj, audio.uri);
+            }
+        }
+
+        context.updateState(context, {
+            soundObj: status,
+            currentAudio: audio,
+            isPlaying: true,
+            currentAudioIndex: index,
+            playbackObj: context.playbackObj,
+        });
+        storeAudioForNextOpening(audio, index);
+    }
+
+    if (!context.currentAudio) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.audioIndex}>{`${context.currentAudioIndex + 1} / ${context.totalAudioCount}`}</Text>
+            <View style={styles.midBannerContainer}>
+                <MaterialCommunityIcons
+                    name="music-circle"
+                    size={300}
+                    color={context.isPlaying ? color.ACTIVE_BG : color.FONT_LIGHT} />
+            </View>
+            <View style={styles.audioPlayerContainer}>
+                <Text numberOfLines={1} style={styles.audioTitle} >{context.currentAudio.filename}</Text>
+                <Slider
+                    style={{ width: width, height: 40 }}
+                    minimumValue={0}
+                    maximumValue={1}
+                    value={calculateSeekBar()}
+                    minimumTrackTintColor={color.FONT_MEDIUM}
+                    maximumTrackTintColor={color.ACTIVE_BG}
+                />
+                <View style={styles.audioControllers}>
+                    <PlayerButton
+                        onPress={handlePrevious}
+                        iconType="backward" />
+                    <PlayerButton
+                        onPress={handlePlayPause}
+                        iconType={context.isPlaying ? "pause" : "play"} />
+                    <PlayerButton
+                        onPress={handleNext}
+                        iconType="forward" />
+                </View>
+            </View>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    audioControllers: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        paddingBottom: 20,
+        marginHorizontal: 60
+    },
+    audioCount: {
+        color: color.FONT_LIGHT,
+        padding: 15,
+        textAlign: 'right',
+        fontSize: 14
+    },
+    midBannerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    audioTitle: {
+        color: color.FONT,
+        fontSize: 16,
+        padding: 15
+    },
+});
+
+//make this component available to the app
+export default Player;
