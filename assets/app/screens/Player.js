2f12182791d5f84431e89ce2a145e0fdd1f11c0e
Carlos Rodriguez <crribera@gmail.com>
2023-02-08 18:21:29 +0100

    added play/pause functionality on player page

--- assets/app/screens/Player.js
+++ assets/app/screens/Player.js
@@ -5,6 +5,7 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import Slider from '@react-native-community/slider';
 import PlayerButton from '../components/PlayerButton';
 import { AudioContext } from '../context/AudioProvider';
+import { play, pause, resume } from '../misc/AudioController';
 
 const { width } = Dimensions.get('window');
 
@@ -23,68 +24,100 @@ const Player = () => {
         context.loadPreviousAudio();
     }, []);
 
-    if (!context.currentAudio) {
-        return null;
+    const handlePlayPause = async () => {
+        // play audio
+        if (context.soundObj === null) {
+            const audio = context.currentAudio;
+            const status = await play(context.playbackObj, audio.uri);
+            return context.updateState(context, {
+                soundObj: status,
+                currentAudio: audio,
+                isPlaying: true,
+                currentAudioIndex: context.currentAudioIndex,
+            });
+        }
+        // pause audio
+        if (context.soundObj && context.soundObj.isPlaying) {
+            const status = await pause(context.playbackObj);
+            return context.updateState(context, {
+                soundObj: status,
+                isPlaying: false,
+            });
+        }
+        // resume audio
+        if (context.soundObj && !context.soundObj.isPlaying) {
+            const status = await resume(context.playbackObj);
+            return context.updateState(context, {
+                soundObj: status,
+                isPlaying: true,
+            });
+        }
     }
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.audioIndex}>{`${context.currentAudioIndex + 1} / ${context.totalAudioCount}`}</Text>
-            <View style={styles.midBannerContainer}>
-                <MaterialCommunityIcons
-                    name="music-circle"
-                    size={300}
-                    color={context.isPlaying ? color.ACTIVE_BG : color.FONT_LIGHT} />
-            </View>
-            <View style={styles.audioPlayerContainer}>
-                <Text numberOfLines={1} style={styles.audioTitle} >{context.currentAudio.filename}</Text>
-                <Slider
-                    style={{ width: width, height: 40 }}
-                    minimumValue={0}
-                    maximumValue={1}
-                    value={calculateSeekBar()}
-                    minimumTrackTintColor={color.FONT_MEDIUM}
-                    maximumTrackTintColor={color.ACTIVE_BG}
-                />
-                <View style={styles.audioControllers}>
-                    <PlayerButton iconType="backward" />
-                    <PlayerButton onPress={() => console.log("play pressed")} iconType={context.isPlaying ? "pause" : "play"} />
-                    <PlayerButton iconType="forward" />
+
+            if (!context.currentAudio) {
+                return null;
+            }
+
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.audioIndex}>{`${context.currentAudioIndex + 1} / ${context.totalAudioCount}`}</Text>
+                    <View style={styles.midBannerContainer}>
+                        <MaterialCommunityIcons
+                            name="music-circle"
+                            size={300}
+                            color={context.isPlaying ? color.ACTIVE_BG : color.FONT_LIGHT} />
+                    </View>
+                    <View style={styles.audioPlayerContainer}>
+                        <Text numberOfLines={1} style={styles.audioTitle} >{context.currentAudio.filename}</Text>
+                        <Slider
+                            style={{ width: width, height: 40 }}
+                            minimumValue={0}
+                            maximumValue={1}
+                            value={calculateSeekBar()}
+                            minimumTrackTintColor={color.FONT_MEDIUM}
+                            maximumTrackTintColor={color.ACTIVE_BG}
+                        />
+                        <View style={styles.audioControllers}>
+                            <PlayerButton iconType="backward" />
+                            <PlayerButton onPress={handlePlayPause}
+                                iconType={context.isPlaying ? "pause" : "play"} />
+                            <PlayerButton iconType="forward" />
+                        </View>
+                    </View>
                 </View>
-            </View>
-        </View>
-    );
-};
+            );
+        };
 
-// define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    audioControllers: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        paddingBottom: 20,
-        marginHorizontal: 60
-    },
-    audioCount: {
-        color: color.FONT_LIGHT,
-        padding: 15,
-        textAlign: 'right',
-        fontSize: 14
-    },
-    midBannerContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    audioTitle: {
-        color: color.FONT,
-        fontSize: 16,
-        padding: 15
-    },
-});
+        // define your styles
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1
+            },
+            audioControllers: {
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                paddingBottom: 20,
+                marginHorizontal: 60
+            },
+            audioCount: {
+                color: color.FONT_LIGHT,
+                padding: 15,
+                textAlign: 'right',
+                fontSize: 14
+            },
+            midBannerContainer: {
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            audioTitle: {
+                color: color.FONT,
+                fontSize: 16,
+                padding: 15
+            },
+        });
 
-//make this component available to the app
-export default Player;
+        //make this component available to the app
+        export default Player;
